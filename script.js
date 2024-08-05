7dadb832563e3f993011e393f962ef5af88f2fda
anshika0704 <anshikachoudhary0704@gmail.com>
2024-08-06 01:04:58 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,64 @@
+//toggle icon navbar
+let menuIcon = document.querySelector('#menu-icon');
+let navbar = document.querySelector('.navbar');
+
+menuIcon.onclick = () => {
+  menuIcon.classList.toggle('bx-x');
+  navbar.classList.toggle('active');
+}
+
+//scroll sections
+let sections = document.querySelectorAll('section');
+let navLinks = document.querySelectorAll('header nav a');
+
+window.onscroll = () => {
+  sections.forEach(sec => {
+    let top = window.scrollY;
+    let offset = sec.offsetTop - 100;
+    let height = sec.offsetHeight;
+    let id = sec.getAttribute('id');
+
+    if(top >= offset && top < offset + height) {
+      //active navbar links
+      navLinks.forEach(links => {
+        links.classList.remove('active');
+        document.querySelector('header nav a[href*=' + id + ']').classList.add('active');
+      });
+      //active sections for animation on scroll
+      sec.classList.add('show-animate');
+    }
+    else{
+      sec.classList.remove('show-animate');
+    }
+  })
+
+  //sticky header
+  let header = document.querySelector('header');
+
+  header.classList.toggle('sticky', window.scrollY > 100);
+
+  //remove toggle icon and navbar when click navbar links (scroll)
+  menuIcon.classList.remove('bx-x');
+  navbar.classList.remove('active');
+
+  let footer = document.querySelector('footer');
+
+  footer.classList.toggle('show-animate', this.innerHeight + this.scrollY >= document.scrollingElement.scrollHeight);
+}
+
+document.getElementById('knowMore1').addEventListener('click', function() {
+  document.getElementById('about').scrollIntoView({ behavior: 'smooth' });
+});
+
+document.getElementById('knowMore2').addEventListener('click', function() {
+  document.getElementById('education').scrollIntoView({ behavior: 'smooth' });
+});
+
+document.getElementById('letsTalk').addEventListener('click', function() {
+  document.getElementById('contact').scrollIntoView({ behavior: 'smooth' });
+});
+
+
 //turn pages when click next or prev button
 const pageTurnBtn = document.querySelectorAll('.nextprev-btn');
 
@@ -41,11 +102,11 @@ commentBtn.onclick = () => {
 //create reverse index function
 let totalPages = pages.length;
 let pageNumber = 0;
+
 function reverseIndex() {
   pageNumber--;
   if (pageNumber < 0) {
     pageNumber = totalPages - 1;
-
   }
 }
 
@@ -67,12 +128,56 @@ backProfileBtn.onclick = () => {
   })
 }
 
+//opening book animation
+const coveRight = document.querySelector('.cover.cover-right');
+const pageLeft = document.querySelector('.book-page.page-left');
+
+//opening book animation (cover right animation)
+setTimeout(() => {
+  coveRight.classList.add('turn');
+}, 2100)
+
+setTimeout(() => {
+  coveRight.style.zIndex = -1;
+}, 2800)
+
+//opening book animation (page left or profile page animation)
+setTimeout(() => {
+  pageLeft.style.zIndex = 20;
+}, 3200)
 
 
+//opening book animation (all page right animation)
+pages.forEach((_, index) => {
+  setTimeout(() => {
+    reverseIndex();
+    pages[pageNumber].classList.remove('turn');
+
+    setTimeout(() => {
+      reverseIndex();
+      pages[pageNumber].style.zIndex = 10 + index;
+    }, 500)
+
+  }, (index + 1) * 200 + 2100)
+})
 
 
+document.addEventListener("DOMContentLoaded", function() {
+  const commentButton = document.querySelector('.button.comment');
+  const commentPage = document.querySelector('.comment-box');
 
+  commentButton.addEventListener('click', function(event) {
+      event.preventDefault();
+      commentPage.scrollIntoView({ behavior: 'smooth' });
+  });
+});
 
+document.addEventListener("DOMContentLoaded", function() {
+  const profileButton = document.querySelector('back-profile');
+  const profilePage = document.querySelector('.profile-page');
 
-
-
+  profileButton.addEventListener('click', function(event) {
+      event.preventDefault();
+      profilePage.scrollIntoView({ behavior: 'smooth' });
+  });
+});
